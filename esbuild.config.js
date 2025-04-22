1900ffc8c311fba923ed0c920ec958aae483fab1
ChristopherJoshy <christopherjoshy4@gmail.com>
2025-04-22 11:50:40 +0530

    uhhhh IDK what to say

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,8 +4,8 @@ await esbuild.build({
   entryPoints: ['server/index.ts'],
   bundle: true,
   platform: 'node',
-  target: 'node18',
+  target: ['node18'],
   outdir: 'dist',
   format: 'esm',
-  external: ['express', 'ws', 'sqlite3', 'bcryptjs', 'passport', 'express-session'],
+  external: ['express', ...Object.keys(JSON.parse(await import('fs').then(fs => fs.promises.readFile('package.json', 'utf8'))).dependencies)]
 }) 
